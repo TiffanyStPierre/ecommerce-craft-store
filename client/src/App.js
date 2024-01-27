e37cb84f0dca0a544fca0028229144e3dae32412
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-01-27 08:16:43 -0700

    Set up React Router Skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
-
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
-
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+    </>
+  );
 }
 
 export default App;
+
+
+// fetchData = () => {
+//   axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+//   .then((response) => {
+//     // handle success
+//     console.log(response.data) // The entire response from the Rails API
+
+//     console.log(response.data.message) // Just the message
+//     this.setState({
+//       message: response.data.message
+//     });
+//   }) 
+// }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <p>Crafting Adventures and Endless Creativity:</p>
+      <p>Your Journey Awaits.</p>
+      <button>Start Exploring</button>
+    </div>
+  )
+}
