5105709a52533ba485e865260d71c1ccaff112df
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-01-27 07:27:49 -0700

    Initial project setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Set the ActiveSupport cache format version.
+Rails.application.configure do
+  config.active_support.cache_format_version = 7.1
+end
