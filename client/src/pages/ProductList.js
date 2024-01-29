31c8c911aab7629226272e23b0092a4adc1c3014
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-01-29 14:54:36 -0700

    Set up axios request for all product data

--- client/src/pages/ProductList.js
+++ client/src/pages/ProductList.js
@@ -5,11 +5,32 @@ import ProductListItem from "../components/ProductListItem";
 
 export default function ProductList() {
   const { category } = useParams();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        // Construct the API endpoint based on the category
+        const apiEndpoint = category === 'all' ? '/api/products' : `/api/products/${category}`;
+
+        const response = await axios.get(apiEndpoint);
+        console.log(response.data);
+        setProducts(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    // Call the fetchProducts function on page load
+    fetchProducts();
+  }, [category]); // Include category in the dependency array to re-run the effect when it changes
 
   return (
     <>
       <h1>Product List Page</h1>
-      <ProductListItem />
+      {products.map((product) => (
+        <ProductListItem key={product.id} product={product} />
+      ))}
     </>
   );
 }
