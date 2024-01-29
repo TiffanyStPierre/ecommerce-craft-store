bf97afc4c85b0f1bdb58ca1fc077ac8346975afa
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-01-29 15:33:11 -0700

    Finish styling of ProductList

--- client/src/components/ProductListItem.js
+++ client/src/components/ProductListItem.js
@@ -3,11 +3,11 @@ import Card from "react-bootstrap/Card";
 
 export default function ProductListItem(props) {
   return (
-    <Card style={{ width: '250px' }}>
-      <Card.Img variant="top" src={props.product.thumbnail_url} style={{height: '250px'}}/>
-      <Card.Body className="d-flex flex-column align-items-center">
+    <Card style={{ width: '350px', height: '400px' }} className="m-4">
+      <Card.Img variant="top" src={props.product.image_url} style={{height: '225px', width: '350px'}}/>
+      <Card.Body className="d-flex flex-column align-items-center bg-light">
         <Card.Title className="text-center">{props.product.name}</Card.Title>
-        <Card.Text className="mb-3">
+        <Card.Text className="mb-4 mt-2 fs-5">
           ${props.product.price}
         </Card.Text>
         <Button className="custom-button mb-2">Details</Button>
--- client/src/pages/ProductList.js
+++ client/src/pages/ProductList.js
@@ -11,7 +11,8 @@ export default function ProductList() {
     const fetchProducts = async () => {
       try {
         // Construct the API endpoint based on the category
-        const apiEndpoint = category === 'all' ? '/api/products' : `/api/products/${category}`;
+        const apiEndpoint =
+          category === "all" ? "/api/products" : `/api/products/${category}`;
 
         const response = await axios.get(apiEndpoint);
         console.log(response.data);
@@ -27,10 +28,15 @@ export default function ProductList() {
 
   return (
     <>
-      <h1>Product List Page</h1>
-      {products.map((product) => (
-        <ProductListItem key={product.id} product={product} />
-      ))}
+      <h2 className="page-subtitle">Product List Page</h2>
+      <div className="mx-auto" style={{width: "70%"}}>
+        <div className="d-flex flex-wrap align-items-center justify-content-between">
+          {products.map((product) => (
+            <ProductListItem key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+      <div className="page-footer-buffer"></div>
     </>
   );
 }
