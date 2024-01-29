e248a62b883cc98f78f7ed5bdd2968babb8d2280
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-01-29 15:07:29 -0700

    Display data and style ProductListItem

--- client/src/components/ProductListItem.js
+++ client/src/components/ProductListItem.js
@@ -1,5 +1,17 @@
-export default function ProductListItem() {
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+export default function ProductListItem(props) {
   return (
-    <h1>Product List Item Component</h1>
+    <Card style={{ width: '250px' }}>
+      <Card.Img variant="top" src={props.product.thumbnail_url} style={{height: '250px'}}/>
+      <Card.Body className="d-flex flex-column align-items-center">
+        <Card.Title className="text-center">{props.product.name}</Card.Title>
+        <Card.Text className="mb-3">
+          ${props.product.price}
+        </Card.Text>
+        <Button className="custom-button mb-2">Details</Button>
+      </Card.Body>
+    </Card>
   )
 }
