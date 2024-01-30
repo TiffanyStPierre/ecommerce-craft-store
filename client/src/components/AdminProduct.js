220f0d0bc357cb03124ce030be89d0518be04e9a
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-01-30 06:49:31 -0700

    Change admin products page display from table to grid

--- client/src/components/AdminProduct.js
+++ client/src/components/AdminProduct.js
@@ -1,20 +1,45 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Card, Col, Row } from "react-bootstrap";
+import "../styles/admin.css";
 
 export default function AdminProduct(props) {
+
   return (
-    <tr>
-      <td>
-        <img src={props.product.thumbnail_url} alt={props.product.name} style={{height: "100px", width: "100px"}} className="rounded-circle"/>
-      </td>
-      <td>{props.product.name}</td>
-      <td>${props.product.price}</td>
-      <td>Category</td>
-      <td>{props.product.inventory}</td>
-      <td>Promotions</td>
-      <td><FontAwesomeIcon icon={faPen} /></td>
-      <td><FontAwesomeIcon icon={faTrash} className="text-danger"/></td>
-    </tr>
+    <>
+      {/* Displayed on smaller screens */}
+      <Row className="mb-2 d-md-none">
+        <Col xs={12} sm={12} md={8} lg={8}>
+          <Card className="admin-product-card">
+            <Card.Img variant="top" src={props.product.thumbnail_url} alt={props.product.name} />
+            <Card.Body>
+              <Card.Title>{props.product.name}</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      {/* Displayed on larger screens */}
+      <Row className="mb-3 d-none d-md-flex">
+        <Col>
+          <img src={props.product.thumbnail_url} alt={props.product.name} style={{ height: "100px", width: "100%" }} className="rounded-circle" />
+        </Col>
+        <Col>
+          {props.product.name}
+        </Col>
+        <Col>
+          ${props.product.price}
+        </Col>
+        <Col>
+          Category
+        </Col>
+        <Col>
+          {props.product.inventory}
+        </Col>
+        <Col>
+          Promotions
+        </Col>
+      </Row>
+    </>
   );
 }
--- client/src/pages/AdminProducts.js
+++ client/src/pages/AdminProducts.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import AdminProduct from "../components/AdminProduct";
-import Table from "react-bootstrap/Table";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function AdminProducts() {
   const [products, setProducts] = useState([]);
@@ -24,25 +24,30 @@ export default function AdminProducts() {
     <>
       <h2 className="page-subtitle">Admin Dashboard - Product List</h2>
       <h3>Products</h3>
-      <Table striped bordered size="sm" className="mt-5 mx-auto" style={{width: "70%"}}>
-        <thead>
-          <tr>
-            <th></th>
-            <th className="h5">Name</th>
-            <th className="h5">Price</th>
-            <th className="h5">Category</th>
-            <th className="h5">Inventory</th>
-            <th className="h5">Promotions</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <AdminProduct key={product.id} product={product} />
-          ))}
-        </tbody>
-      </Table>
+      <Container className="mt-5">
+        <Row className="d-none d-md-flex">
+          <Col>
+          </Col>
+          <Col>
+            <strong>Name</strong>
+          </Col>
+          <Col>
+            <strong>Price</strong>
+          </Col>
+          <Col>
+            <strong>Category</strong>
+          </Col>
+          <Col>
+            <strong>Inventory</strong>
+          </Col>
+          <Col>
+            <strong>Promotions</strong>
+          </Col>
+        </Row>
+        {products.map((product) => (
+          <AdminProduct key={product.id} product={product} />
+        ))}
+      </Container>
       <div className="page-footer-buffer"></div>
     </>
   );
