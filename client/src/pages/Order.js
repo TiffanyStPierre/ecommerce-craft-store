02d842e01175a03292639a3fc7ee92221fab53a2
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-01-30 12:38:20 -0700

    Access some order data to display on order details page - not complete

--- client/src/pages/Order.js
+++ client/src/pages/Order.js
@@ -3,9 +3,20 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 export default function Order() {
-
   const { id } = useParams();
-  const [order, setOrder] = useState([]);
+  const [order, setOrder] = useState({
+    id: "",
+    customer_id: "",
+    customer: {},
+    order_date: "",
+    products: [],
+    shipped_date: "",
+    subtotal_amount: "",
+    tax_amount: "",
+    total_amount: "",
+    updated_at: "",
+    created_at: "",
+  });
 
   useEffect(() => {
     const fetchOrder = async () => {
@@ -26,6 +37,35 @@ export default function Order() {
     <>
       <h2 className="page-subtitle">Admin Dashboard - Order Details</h2>
       <h3>Customer Order Details</h3>
+      <div>
+        <div>
+          <h4>Customer</h4>
+          <p>
+            {order.customer.first_name} {order.customer.last_name}
+            <br />
+            {order.customer.street_address}
+            <br />
+            {order.customer.city}, {order.customer.province}
+            <br />
+            {order.customer.postal_code}
+          </p>
+        </div>
+        <div>
+          <h4>Order Date</h4>
+          <p>{order.order_date}</p>
+        </div>
+        <div>
+          <h4>Invoice #</h4>
+          <p>{order.id}</p>
+        </div>
+        <div>
+          <h4>Order Total</h4>
+          <p>${order.total_amount}</p>
+        </div>
+        <div>
+          <h4>Items Ordered</h4>
+        </div>
+      </div>
     </>
   );
 }
