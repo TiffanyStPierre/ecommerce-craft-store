949f4afd2f99af761f78e7ca2e6f6280fabadb26
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-01-31 08:46:41 -0700

    Add styling to create product page

--- client/src/pages/CreateProduct.js
+++ client/src/pages/CreateProduct.js
@@ -8,20 +8,14 @@ export default function CreateProduct() {
   const [productData, setProductData] = useState({
     name: "",
     description: "",
-    price: 0,
+    price: null,
     category: "",
     imageUrl: "",
     thumbnailUrl: "",
   });
 
-  const {
-    name,
-    description,
-    price,
-    category,
-    imageUrl,
-    thumbnailUrl,
-  } = productData;
+  const { name, description, price, category, imageUrl, thumbnailUrl } =
+    productData;
 
   const onChange = (e) => {
     setProductData((prevState) => ({
@@ -32,8 +26,7 @@ export default function CreateProduct() {
 
   const onSubmit = (e) => {
     e.preventDefault();
-
-  }
+  };
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -52,50 +45,96 @@ export default function CreateProduct() {
   return (
     <>
       <h2 className="page-subtitle">Admin - New Product</h2>
+      <h3 className="mb-5">Create New Product</h3>
       <Form onSubmit={onSubmit}>
-        <Form.Group className="mb-3" controlId="name">
+        <Form.Group className="mb-4 form-input-group mx-auto" controlId="name">
           <Form.Label>Product Name</Form.Label>
-          <Form.Control type="text" name="name" value={name} onChange={onChange} />
+          <Form.Control
+            type="text"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="description">
+        <Form.Group
+          className="mb-4 form-input-group mx-auto"
+          controlId="description"
+        >
           <Form.Label>Description</Form.Label>
-          <Form.Control type="text" as="textarea" rows={5} name="description" value={description} onChange={onChange} />
+          <Form.Control
+            type="text"
+            as="textarea"
+            rows={5}
+            name="description"
+            value={description}
+            onChange={onChange}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="price">
+        <Form.Group className="mb-4 form-input-group mx-auto" controlId="price">
           <Form.Label>Price</Form.Label>
-          <Form.Control type="number" name="price" value={price} onChange={onChange} />
+          <Form.Control
+            type="number"
+            name="price"
+            value={price}
+            onChange={onChange}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="category">
+        <Form.Group
+          className="mb-4 form-input-group mx-auto"
+          controlId="category"
+        >
           <Form.Label>Product Category</Form.Label>
-          <Form.Select aria-label="Category select" name="category" value={category} onChange={onChange}>
+          <Form.Select
+            aria-label="Category select"
+            name="category"
+            value={category}
+            onChange={onChange}
+          >
             {categories &&
               categories.map((category) => <option>{category.name}</option>)}
           </Form.Select>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="imageUrl">
+        <Form.Group
+          className="mb-4 form-input-group mx-auto"
+          controlId="imageUrl"
+        >
           <Form.Label>Product Image URL</Form.Label>
-          <Form.Control type="text" name="imageUrl" value={imageUrl} onChange={onChange} />
+          <Form.Control
+            type="text"
+            name="imageUrl"
+            value={imageUrl}
+            onChange={onChange}
+          />
           <Form.Text className="text-muted">
             Image URL from image storage host. Image size must be 640px x 427px
           </Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="thumbnailUrl">
+        <Form.Group
+          className="mb-4 form-input-group mx-auto"
+          controlId="thumbnailUrl"
+        >
           <Form.Label>Thumbnail Image URL</Form.Label>
-          <Form.Control type="text" name="thumbnailUrl" value={thumbnailUrl} onChange={onChange} />
+          <Form.Control
+            type="text"
+            name="thumbnailUrl"
+            value={thumbnailUrl}
+            onChange={onChange}
+          />
           <Form.Text className="text-muted">
             Thumbnail image URL from image storage host. Image size must be
             200px x 200px
           </Form.Text>
         </Form.Group>
-
-        <Button variant="primary" type="submit" className="custom-button">
-          Create Product
-        </Button>
+        <div className="text-center">
+          <Button variant="primary" type="submit" className="custom-button mt-5">
+            Create Product
+          </Button>
+        </div>
       </Form>
       <div className="page-footer-buffer"></div>
     </>
