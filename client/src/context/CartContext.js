b40a13556b13190cfae02ed01843d2761543b8ec
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-02-06 14:59:30 -0700

    Implemented removeFromCart function

--- client/src/context/CartContext.js
+++ client/src/context/CartContext.js
@@ -25,7 +25,19 @@ export const CartProvider = ({ children }) => {
   }
 
   const removeFromCart = (item) => {
+    const isItemInCart = cartItems.find((cartItem) => cartItem.id === item.id);
 
+    if (isItemInCart.quantity === 1) {
+      setCartItems(cartItems.filter((cartItem) => cartItem.id !== item.id)); // if the quantity of the item is 1, remove the item from the cart
+    } else {
+      setCartItems(
+        cartItems.map((cartItem) =>
+          cartItem.id === item.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 } // if the quantity of the item is greater than 1, decrease the quantity of the item
+            : cartItem
+        )
+      );
+    }
   }
 
   const clearCart = () =>{
