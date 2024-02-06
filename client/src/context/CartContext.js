39cd8bd78cf30e74dae06c688522ce1046870013
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-02-06 05:22:05 -0700

    Set up CartProvider context, including accessing local storage

---
+++ client/src/context/CartContext.js
@@ -0,0 +1,47 @@
+import { createContext, useState, useEffect } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState(
+    localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : []
+  );
+
+  const addToCart = (item) => {
+
+  }
+
+  const removeFromCart = (item) => {
+
+  }
+
+  const clearCart = () =>{
+
+  }
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  useEffect(() => {
+    const cartItems = localStorage.getItem("cartItems");
+    if (cartItems) {
+      setCartItems(JSON.parse(cartItems));
+    }
+  }, []);
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        addToCart,
+        removeFromCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
