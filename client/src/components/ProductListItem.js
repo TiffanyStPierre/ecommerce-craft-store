abb4f1ff63e224df23aabd7477db465dca6a59f9
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-02-10 13:05:46 -0700

    Add sold out badge to product cards in product list

--- client/src/components/ProductListItem.js
+++ client/src/components/ProductListItem.js
@@ -1,20 +1,35 @@
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import { Card, Badge } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function ProductListItem(props) {
   return (
-  <Link to={`/product/${props.product.id}`} className="link">
-    <Card style={{ width: '300px', height: '365px' }} className="my-4 mx-3">
-      <Card.Img variant="top" src={props.product.image_url} style={{height: '225px', width: '350px'}}/>
-      <Card.Body className="d-flex flex-column align-items-center bg-light">
-        <Card.Title className="text-center fs-6">{props.product.name}</Card.Title>
-        <Card.Text className="mb-3 mt-1 fs-6">
-          ${props.product.price}
-        </Card.Text>
-        <Button className="custom-button mb-2">Details</Button>
-      </Card.Body>
-    </Card>
+    <Link to={`/product/${props.product.id}`} className="link">
+      <Card
+        style={{ width: "300px", height: "365px" }}
+        className="my-4 mx-3 position-relative"
+      >
+        {props.product && !props.product.inventory && (
+          <div className="position-absolute top-0 start-0 m-2">
+            <Badge bg="danger">Sold Out</Badge>
+          </div>
+        )}
+        <Card.Img
+          variant="top"
+          src={props.product.image_url}
+          style={{ height: "225px", width: "350px" }}
+        />
+
+        <Card.Body className="d-flex flex-column align-items-center bg-light">
+          <Card.Title className="text-center fs-6">
+            {props.product.name}
+          </Card.Title>
+          <Card.Text className="mb-3 mt-1 fs-6">
+            ${props.product.price}
+          </Card.Text>
+          <Button className="custom-button mb-2">Details</Button>
+        </Card.Body>
+      </Card>
     </Link>
-  )
+  );
 }
