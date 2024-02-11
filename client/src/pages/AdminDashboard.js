7da4853c1b92f8abbc93d825bcb3a449b3f8afdb
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-02-10 20:32:00 -0700

    Update styling on admin dashboard to move footer down page

--- client/src/pages/AdminDashboard.js
+++ client/src/pages/AdminDashboard.js
@@ -3,38 +3,38 @@ import { Link } from "react-router-dom";
 
 export default function AdminDashboard() {
   return (
-    <>
+    <div className="d-flex flex-column" style={{ minHeight: "80vh" }}>
       <h2 className="page-subtitle">Admin Dashboard</h2>
       <div
-        className="d-flex mx-auto align-items-center justify-content-around"
+        className="d-flex flex-grow-1 mx-auto align-items-center justify-content-around"
         style={{ width: "70%" }}
       >
         <Link to="/admin/products" className="link">
-          <Card>
+          <Card className="py-4">
             <Card.Body>
-              <Card.Title>Products</Card.Title>
+              <Card.Title className="text-center">Products</Card.Title>
               <Card.Text>Click here to view and manage products</Card.Text>
             </Card.Body>
           </Card>
         </Link>
         <Link to="/admin/promotions" className="link">
-          <Card>
+          <Card className="py-4">
             <Card.Body>
-              <Card.Title>Promotions</Card.Title>
+              <Card.Title className="text-center">Promotions</Card.Title>
               <Card.Text>Click here to view and manage promotions</Card.Text>
             </Card.Body>
           </Card>
         </Link>
         <Link to="/admin/orders" className="link">
-          <Card>
+          <Card className="py-4">
             <Card.Body>
-              <Card.Title>Orders</Card.Title>
+              <Card.Title className="text-center">Orders</Card.Title>
               <Card.Text>Click here to view and manage orders</Card.Text>
             </Card.Body>
           </Card>
         </Link>
       </div>
       <div className="page-footer-buffer"></div>
-    </>
+    </div>
   );
 }
