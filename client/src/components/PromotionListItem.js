d5aa2665856d15e59eedd19218d9fd65ee18ad63
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-02-12 11:10:16 -0700

    Add colored badges to promotion list item with various promotion statuses

--- client/src/components/PromotionListItem.js
+++ client/src/components/PromotionListItem.js
@@ -1,9 +1,7 @@
-import { Card, Col, Row } from "react-bootstrap";
+import { Card, Col, Row, Badge } from "react-bootstrap";
 import "../styles/admin.css";
 
 export default function PromotionListItem(props) {
-  
-
   return (
     <>
       {/* Displayed on smaller screens */}
@@ -16,13 +14,19 @@ export default function PromotionListItem(props) {
           <Card className="admin-product-card mx-auto">
             <Card.Body>
               <Card.Header className="text-center" as="h4">
-              {props.promotion.name}
+                {props.promotion.name}
               </Card.Header>
               <Card.Title className="text-center my-3">
-              {Math.round(props.promotion.percent_discount)}% Discount
+                {Math.round(props.promotion.percent_discount)}% Discount
               </Card.Title>
               <Card.Subtitle className="text-center my-1">
-                Start Date: {props.promotion.start_date ? props.promotion.start_date : "None"}<br/>End Date: {props.promotion.end_date ? props.promotion.end_date : "None"}
+                Start Date:{" "}
+                {props.promotion.start_date
+                  ? props.promotion.start_date
+                  : "None"}
+                <br />
+                End Date:{" "}
+                {props.promotion.end_date ? props.promotion.end_date : "None"}
               </Card.Subtitle>
             </Card.Body>
           </Card>
@@ -33,20 +37,28 @@ export default function PromotionListItem(props) {
         className="mb-3 d-none d-md-flex align-items-center border rounded p-2 admin-product-item"
         onClick={() => props.onClick("sm-down", props.promotion)}
       >
-        <Col xs={3} md={3} className="text-center">
+        <Col xs={3} md={2} className="text-center">
           {props.promotion.name}
         </Col>
-        <Col xs={3} md={3} className="text-center">
-        {Math.round(props.promotion.percent_discount)}%
+        <Col xs={3} md={2} className="text-center">
+          {Math.round(props.promotion.percent_discount)}%
         </Col>
-        <Col xs={3} md={3} className="text-center">
-        {props.promotion.finished ? "Finished" : "Not Finished"}
+        <Col xs={3} md={2} className="text-center">
+          {props.promotion.finished ? (
+            <Badge bg="danger">Finished</Badge>
+          ) : props.promotion.active ? (
+            <Badge bg="success">Active</Badge>
+          ) : props.promotion.upcoming ? (
+            <Badge bg="secondary">Upcoming</Badge>
+          ) : (
+            ""
+          )}
         </Col>
-        <Col xs={3} md={3} className="text-center">
-        {props.promotion.start_date ? props.promotion.start_date : "None"}
+        <Col xs={3} md={2} className="text-center">
+          {props.promotion.start_date ? props.promotion.start_date : "None"}
         </Col>
-        <Col xs={3} md={3} className="text-center">
-        {props.promotion.end_date ? props.promotion.end_date : "None"}
+        <Col xs={3} md={2} className="text-center">
+          {props.promotion.end_date ? props.promotion.end_date : "None"}
         </Col>
       </Row>
     </>
