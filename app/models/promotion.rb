01cc1e2f847e471096fedabd3414be152f010854
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-02-12 10:43:33 -0700

    Add methods to promotion model to determine status of promotions

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,3 +1,16 @@
 class Promotion < ApplicationRecord
   has_and_belongs_to_many :products, join_table: 'promotions_products'
+
+  def finished?
+    end_date < Date.current
+  end
+
+  def upcoming?
+    start_date > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
+  
 end
